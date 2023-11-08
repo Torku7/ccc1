3105ae21bc6c6987fc30a1e98bcdd64cb358e673
mnyu123 <whdudwo5582@gmail.com>
2023-11-08 21:36:48 +0900

    패키지 분리 , 일부 파일 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    // 여기다가 직접 html 코드를 작성해서 웹 화면 구성하지 말것.
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>책책책 프로젝트 리액트 이관</h1>
     </div>
   );
 }
